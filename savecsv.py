41f1f8ff845c62331f14338ac5460980b3dee514
d4dipdas <84081671+d4dipdas@users.noreply.github.com>
2024-03-17 15:11:06 +0530

    Update savecsv.py

--- savecsv.py
+++ savecsv.py
@@ -10,8 +10,7 @@ parquet_df = spark.read.parquet("/user/maria_dev/dip")
 
 # Write the DataFrame to a CSV file
 parquet_df.write \
-    .option("header", "true") \ 
-    .csv("/user/maria_dev/dip_csv")
+    .option("header", "true").csv("/user/maria_dev/dip_csv")
 
 # Stop the SparkSession
 spark.stop()
