986fa77891e58c8ff0fd396211df9bda36f05b8a
d4dipdas <84081671+d4dipdas@users.noreply.github.com>
2024-03-17 15:09:32 +0530

    Add files via upload

---
+++ savecsv.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("Parquet to CSV Conversion") \
+    .getOrCreate()
+
+# Read the Parquet file
+parquet_df = spark.read.parquet("/user/maria_dev/dip")
+
+# Write the DataFrame to a CSV file
+parquet_df.write \
+    .option("header", "true") \ 
+    .csv("/user/maria_dev/dip_csv")
+
+# Stop the SparkSession
+spark.stop()
